3507ea75760abf68e32157d16d15e1e6bb4996e7
Vivienne Franziska Kreitmair <50141977+vivivanilla@users.noreply.github.com>
2024-02-07 17:31:45 +0100

    Remove deleted file

--- src/main/scala/com/github/vivivanilla/kubediagnose/ListHelper.scala
+++
@@ -1,7 +0,0 @@
-package com.github.vivivanilla.kubediagnose
-
-import scala.jdk.CollectionConverters.*
-
-extension [A](list: java.util.List[A])
-  def convertToScala: Seq[A] =
-    Option(list).map(_.asScala).toSeq.flatten
